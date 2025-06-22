19cea03a6d8560939d973d6025c7d3f4886eff91
hbstella92 <hbstella92@gmail.com>
2025-06-22 15:51:57 +0900

    Connect spark application and postgresql db

--- spark/consume_stream.py
+++ spark/consume_stream.py
@@ -2,11 +2,31 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import *
 
+jdbc_url = "jdbc:postgresql://localhost:5432/webtoon_db"
+jdbc_properties = {
+    "user": "byeolong2",
+    "password": "cae123!@#",
+    "driver": "org.postgresql.Driver"
+}
+
+def write_to_db(batch_df, batch_id):
+    try:
+        batch_df.write.jdbc(
+            url=jdbc_url,
+            table="user_events",
+            mode="append",
+            properties=jdbc_properties
+        )
+    except Exception as e:
+        print(f"[ERROR] : {e}")
+        raise e
+
 if __name__ == "__main__":
     ss: SparkSession = SparkSession.builder \
     .master("local") \
     .appName("Kafka Consumer User Flow") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0") \
+    .config("spark.jars", "../jars/postgresql-42.7.7.jar") \
     .getOrCreate()
 
     ss.sparkContext.setLogLevel("WARN")
@@ -37,9 +57,8 @@ if __name__ == "__main__":
     .select("data.*")
 
     query = df_parsed.writeStream \
+    .foreachBatch(write_to_db) \
     .outputMode("append") \
-    .format("console") \
-    .option("truncate", False) \
     .start()
 
     query.awaitTermination()
